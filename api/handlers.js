81eac61300da730c05adb92241173185ac8525ab
Daria <daria.shtanko@gmail.com>
2020-05-18 17:31:38 +0200

    Can't figure out where the error is.Can not run because of this error (ReferenceError: api is not defined     at Object.<anonymous> (C:\Users\daria\OneDrive\Документы\HYF\hack-my-future\new\textidor-refactor\index.js:113:17)

--- api/handlers.js
+++ api/handlers.js
@@ -1,12 +1,77 @@
-const fs = require('fs');
-const path = require('path');
-const config = require('../config');
+const fs = require("fs");
+const path = require("path");
+const config = require("../config");
 
 // define FILES_DIR
-const FILES_DIR = path.join(_);
+const FILES_DIR = path.join(__dirname, config.FILES_DIR);
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const unlink = util.promisify(fs.unlink);
+const readDir = util.promisify(fs.readdir);
 
 // declare the handlers
-const handlers = {};
+const handlers = {
+  getFiles: async (req, res, next) => {
+    try {
+      const list = await readDir(FILES_DIR);
+      console.log(FILES_DIR);
+      res.json(list);
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        console.log(FILES_DIR);
+        res.status(404).end();
+      }
+      console.error(err);
+      next(err);
+    }
+  },
+
+  getFileNames: async (req, res, next) => {
+    const fileName = req.params.name;
+    try {
+      const fileText = await readFile(`${FILES_DIR}/${fileName}`, "utf-8");
+      const responseData = {
+        name: fileName,
+        text: fileText,
+      };
+      res.json(responseData);
+    } catch (err) {
+      if (err.code === "ENOENT") res.status(404).end();
+      console.error(err);
+      next(err);
+    }
+  },
+
+  postFileName: async (req, res, next) => {
+    const fileName = req.params.name;
+    const fileText = req.body.text;
+    try {
+      await writeFile(`${FILES_DIR}/${fileName}`, fileText);
+
+      // refactor hint:
+      res.redirect(303, "/api/files");
+      // handlers.getFiles(req, res, next);
+    } catch (err) {
+      if (err.code === "ENOENT") res.status(404).end();
+      console.error(err);
+      next(err);
+    }
+  },
+
+  deleteFileName: async (req, res, next) => {
+    const fileName = req.params.name;
+    try {
+      await unlink(`${FILES_DIR}/${fileName}`);
+
+      // refactor hint:
+      res.redirect(303, "/api/files");
+      // handlers.getFiles(req, res, next);
+    } catch (err) {
+      console.error(err);
+    }
+  },
+};
 
 // export the handlers
-_;
+module.exports = handlers;
--- api/routes.js
+++ api/routes.js
@@ -1,15 +1,22 @@
 // require the handlers
-_;
-const express = require('express');
+const handlers = require("./handlers.js");
+const express = require("express");
 
 // build the router
-_;
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('files API!');
+router.get("/", (req, res) => {
+  res.send("files API!");
 });
 
 // add routes to router
+router.get("/files", handlers.getFiles);
+
+router.get("/files/:name", handlers.getFilesName);
+
+router.post("/files/:name", handlers.postFilesName);
+
+router.delete("/files/:name", handlers.deleteFile);
 
 // export the router
-_;
+module.exports = router;
